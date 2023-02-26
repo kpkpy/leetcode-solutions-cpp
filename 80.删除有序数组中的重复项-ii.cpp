04c6cfaee38013e3494b527a91e65da83f2cb11e
kpkpy <110999292+kpkpy@users.noreply.github.com>
2023-02-26 21:37:54 +0800

    20230226

---
+++ 80.删除有序数组中的重复项-ii.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=80 lang=cpp
+ *
+ * [80] 删除有序数组中的重复项 II
+ */
+#include "include.h"
+// @lc code=start
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int flag =0;
+        int len = nums.size();
+        for(auto i=1; i<nums.size(); i++){
+            if(nums[i]==nums[i-1]&&flag==0) flag++;
+            else if(nums[i]==nums[i-1]&&flag==1){
+                nums.erase(nums.begin()+i);
+                len--; i--;
+            }
+            else flag =0;
+        }
+        return len;
+    }
+};
+// @lc code=end
+
