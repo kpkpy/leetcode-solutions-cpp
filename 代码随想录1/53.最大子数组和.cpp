2d4cee00784998f51f8a410f43ed253804a405a4
kpkpy <110999292+kpkpy@users.noreply.github.com>
2023-04-21 15:38:07 +0800

    starting new session

--- 代码随想录1/53.最大子数组和.cpp
+++ 代码随想录1/53.最大子数组和.cpp
@@ -8,14 +8,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum = 0;
-        int res = INT_MIN;
-        for(int i=0; i<nums.size(); i++){
-            sum += nums[i];
-            if(sum>res) res = sum;
-            if(sum<0){
-                sum = 0;
-            }
+        vector<int> dp(nums.size(), 0);
+        if(nums.size()==1) return nums[0];
+        dp[0] = nums[0];
+        int res = dp[0];
+        for(int i=1; i<nums.size(); i++){
+            dp[i] = max(dp[i-1]+nums[i], nums[i]);
+            if(dp[i]>res) res = dp[i];
         }
         return res;
     }
